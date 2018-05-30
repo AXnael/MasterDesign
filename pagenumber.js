c06dc0b13a6523bb30a8875eeb15c35c5162270e
AXnael <axnaelblog@gmail.com>
2018-05-30 10:18:49 +0800

    Update pagenumber.js

--- pagenumber.js
+++ pagenumber.js
@@ -1,11 +1,3 @@
-var disqus_shortname = &#39;axemi&#39;;
-var disqus_blogger_current_url = &quot;<data:blog.canonicalUrl/>&quot;;
-if (!disqus_blogger_current_url.length) {
-disqus_blogger_current_url = &quot;<data:blog.url/>&quot;;
-}
-var disqus_blogger_homepage_url = &quot;<data:blog.homepageUrl/>&quot;;
-var disqus_blogger_canonical_homepage_url = &quot;<data:blog.canonicalHomepageUrl/>&quot;;
-
 var nopage;
 var jenis;
 var nomerhal;
