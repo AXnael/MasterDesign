d0bbeee69912930b3b302e6dada313fce58f82cd
AXnael <axnaelblog@gmail.com>
2019-04-07 19:40:54 +0800

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -302,7 +302,7 @@
         resultsHTML += '<h1 class="' + class_quizScoreRank + '">';
         if ( totalScore > 80 ) {
           //rank 1: 80-100
-          resultsHTML += plugin.config.quizScoreRank_text.a+"asd";
+          resultsHTML += plugin.config.quizScoreRank_text.a+" "+totalScore;
         } else if ( totalScore > 60 ) {
           //rank 2: 60-79
           resultsHTML += plugin.config.quizScoreRank_text.b;
